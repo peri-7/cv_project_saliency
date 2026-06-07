942e0b6dc5d5fb8162f7e223ecfb9acf4254d8af
zoetsouroufli <zoetsouroufli@gmail.com>
2026-06-07 14:51:36 +0300

    Fix syntax error

--- notebooks/mae_smoke_test.py
+++ notebooks/mae_smoke_test.py
@@ -1,5 +1,5 @@
 # 1. Clone your repository directly into the Kaggle working directory
-!git clone https://github.com/peri-7/cv_project_saliency.git
+# !git clone https://github.com/peri-7/cv_project_saliency.git
 
 import sys
 import os
